ff2044e57ceaff044e7a8564d1b4a7a2d52fb1b6
handsomemolly <ryan.kendig@gmail.com>
2020-12-03 15:22:24 -0800

    commit to merge

--- MakeRequest.rb
+++ MakeRequest.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'openssl'
 require 'json'
 
-url = URI("https://api-football-v1.p.rapidapi.com/v2/leagueTable/524")
+url = URI("https://api-football-v1.p.rapidapi.com/v2/leagues/type/league")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
