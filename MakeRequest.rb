5b2851f03776b6e6f09fbaa2356f05b24fec3293
handsomemolly <ryan.kendig@gmail.com>
2020-12-03 11:07:11 -0800

    created standings database pulled from api

--- MakeRequest.rb
+++ MakeRequest.rb
@@ -1,7 +1,25 @@
-require 'rest-client'
+require 'uri'
+require 'net/http'
+require 'openssl'
 require 'json'
 
-url = 
-api = 
+url = URI("https://api-football-v1.p.rapidapi.com/v2/leagueTable/524")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["x-rapidapi-key"] = '680adbf113mshff91628eec25474p194702jsnd1c76f97e364'
+request["x-rapidapi-host"] = 'api-football-v1.p.rapidapi.com'
+
+response = http.request(request)
+
+standings = JSON.parse(response.read_body)
+puts standings
+
+
+
+
 
 
