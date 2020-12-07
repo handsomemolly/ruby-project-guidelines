9eca516d4e6a5c5a0b87b097d42b210a3f53d776
juan_carlos_meza <juancmezat@gmail.com>
2020-12-07 15:20:39 -0800

    Potential menu skeleton

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,19 +1,35 @@
+require_relative 'team.rb'
+require_relative 'match.rb'
+require_relative 'arena.rb'
+
+
 class Menu
 
     def start
         puts 'Hello, we hope you are able to read this'
         puts 'Type "t" to see a list of teams'
-        puts 'Type "stats" to see statistics for the Premier Leagueexi'
-        puts 'Or enter the name of your favorite team'
+        puts 'Type "stats" to see statistics for the Premier League'
+        puts 'Or type the name of your favorite team'
         user_input = STDIN.gets.chomp
-        self.input(unser_input)
+        self.input(user_input)
     end
 
     def input(user_input)
         if user_input == 't'
-            pull list
-        elsif Team.all.include("#{user_input}")
-            send them to another menu
+            puts Team.all.name
+            puts 'Type "stats" to see statistics for the Premier League'
+            puts 'Or type the name of your favorite team'
+        elsif Team.all.name.include("#{user_input}")
+            puts "Home goals: " + (Team.all.find_by(name: "#{user_input}").home_goals).to_s 
+
+            puts "Type 'back' to go back to the main menu"
+            user_input = STDIN.gets.chomp
+            self.input(user_input)
+
+        elsif user_input == 'stats'
+            more stats
+        elsif user_input == 'back'
+            start
         else
             puts "Invalid input. Please enter t to see a list of teams or type the name of your team"
         end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 
-
+menu = Menu.new
+menu.start
 
 puts "HELLO WORLD"
