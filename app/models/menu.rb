6225d1f270cfe4340c0cca6a9ffe81980c06cb60
juan_carlos_meza <juancmezat@gmail.com>
2020-12-07 14:39:41 -0800

    Menu started

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu
     def start
         puts 'Hello, we hope you are able to read this'
         puts 'Type "t" to see a list of teams'
-        puts 'Type "stats" to see statistics for the Premier League'
+        puts 'Type "stats" to see statistics for the Premier Leagueexi'
         puts 'Or enter the name of your favorite team'
         user_input = STDIN.gets.chomp
         self.input(unser_input)
