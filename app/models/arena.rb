f52032f818ff1b59ea336a2fb788e75cd1d0c499
juan_carlos_meza <juancmezat@gmail.com>
2020-12-07 12:00:56 -0800

    Arena methods

--- app/models/arena.rb
+++ app/models/arena.rb
@@ -2,12 +2,18 @@ class Arena < ActiveRecord::Base
     has_many :matches
     has_many :teams, through: :matches
 
+    def matches_here
+        Match.all.select{|m| m.arena_id == self.id}    
+    end
+
     def teams_played_here
-        matches_here = Match.all.select{|m| m.arena_id == self.id}
-        teams_here = matches_here.map{|m| m.away_team_id}
-        teams_here.each do |t|    
-            Team.find_by(id: t)
-        end
+        self.matches_here.map{|m| m.away_team}
+    end
+
+    def goals_scored_here
+        away_goals = self.matches_here.map {|m| m.away_team_goals}.sum
+        home_goals = self.matches_here.map {|m| m.home_team_goals}.sum
+        total_goals = home_goals + away_goals
     end
 
 end
