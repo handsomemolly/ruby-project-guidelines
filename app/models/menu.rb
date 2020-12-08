5c4391ac8f2a93929c9936f70121c13608c67a66
handsomemolly <ryan.kendig@gmail.com>
2020-12-08 00:13:59 -0800

    working CLI without teams

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,22 +4,22 @@ require_relative 'arena.rb'
 
 
     def start
-        puts 'Hello, we hope you are able to read this'
-        puts 'type "options" to see valid commands'
-        team = get_team
+        welcome
         user_input = get_input
+        # team = get_team
         until user_input == "exit"
             case user_input
             when "t" then
-                puts "t"
+                puts Team.all.map{|t| t.name}
                 user_input = get_input
             when "stats" then
                 get_league_stats
                 user_input = get_input
-            when team then
-                get_ranking
-                get_team_stats
-                user_input = get_input
+            # when get_team then
+            #     get_ranking
+            #     get_team_stats
+            #     # user_input = get_input
+            #     start
             when "options" then
                 options
                 user_input = get_input
@@ -54,14 +54,14 @@ require_relative 'arena.rb'
     end
 
     def get_team_stats
-        puts "Goals per game: " + (Team.all.find_by(name: "#{user_input}").goals_per_game).to_s 
-        puts "Win percentage: " + (Team.all.find_by(name: "#{user_input}").win_percentage).to_s
+        puts "Goals per game: " #+ (Team.all.find_by(name: "#{user_input}").goals_per_game).to_s 
+        puts "Win percentage: " #+ (Team.all.find_by(name: "#{user_input}").win_percentage).to_s
     end
 
     def get_league_stats
-        puts "Team with most wins: " + Team.team_with_most_wins.name
-        puts "Team with most away wins: " + Team.team_with_most_away_wins.name
-        puts "Arena with most goals: " + Arena.arena_with_most_goals
+        puts "Team with most wins: " #+ Team.team_with_most_wins.name
+        puts "Team with most away wins: " #+ Team.team_with_most_away_wins.name
+        puts "Arena with most goals: " #+ Arena.arena_with_most_goals
     end
 
     def get_ranking(team)
@@ -76,5 +76,11 @@ require_relative 'arena.rb'
         standings = JSON.parse(response.read_body)
         pp standings['api']['standings'][0].select{|t| t['teamName'] == "Liverpool"}.map{|t| t['rank']}.to_s
     end
+
+    def welcome
+        puts "Welcome to our Broken Project"
+        puts 'type "options" to see valid commands'
+    end
+
 
 
