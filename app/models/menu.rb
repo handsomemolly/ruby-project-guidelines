819e94d95160cfb43e56e45186e645436b4286b2
juan_carlos_meza <juancmezat@gmail.com>
2020-12-07 21:21:37 -0800

    cli working

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -7,14 +7,62 @@ class Menu
 
     def start
         puts 'Hello, we hope you are able to read this'
-        puts 'Type "t" to see a list of teams'
-        puts 'Type "stats" to see statistics for the Premier League'
-        puts 'Or type the name of your favorite team'
-        user_input = STDIN.gets.chomp
-        self.input(user_input)
+        puts 'type "options" to see valid commands'
+        get_input
+        user_input = get_input
+        until user_input == 'exit'
+            case user_input
+                when 't'
+                    puts "#{Team.all.name}"
+                when 'stats'
+                    get_league_stats
+                when get_team
+                    get_ranking
+                    get_team_stats
+                when 'options'
+                    options
+                else 
+                    puts "Invalid command, please try again"
+            end
+        end
+        exit
     end
 
-    def get_ranking(user_input)
+    def options
+        puts 'Type "t" to see a list of teams'
+        puts 'Type "stats" to see statistics for the Premier League'
+        puts 'Type the name of your favorite team in the list'
+        puts 'Type "exit" to leave'
+        get_input
+    end
+
+    def get_input
+        puts 'Please enter a command'
+        STDIN.gets.chomp
+    end
+
+    def get_team
+        puts 'Please enter a comand'
+        team = STDIN.gets.chomp
+        until Team.all.name.include("#{team}")
+            puts 'Please enter the name of a team in the Premier League'
+            team = STDIN.gets.chomp
+        end
+        team
+    end
+
+    def get_team_stats
+        puts "Goals per game: " + (Team.all.find_by(name: "#{user_input}").goals_per_game).to_s 
+        puts "Win percentage: " + (Team.all.find_by(name: "#{user_input}").win_percentage).to_s
+    end
+
+    def get_league_stats
+        puts "Team with most wins: " + Team.team_with_most_wins.name
+        puts "Team with most away wins: " + Team.team_with_most_away_wins.name
+        puts "Arena with most goals: " + Arena.arena_with_most_goals
+    end
+
+    def get_ranking(team)
         url = URI("https://api-football-v1.p.rapidapi.com/v2/leagueTable/2")
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
@@ -26,38 +74,5 @@ class Menu
         standings = JSON.parse(response.read_body)
         pp standings['api']['standings'][0].select{|t| t['teamName'] == "Liverpool"}.map{|t| t['rank']}.to_s
     end
-
-    def input(user_input)
-        if user_input == 't'
-            puts Team.all.name
-            puts 'Type "stats" to see statistics for the Premier League'
-            puts 'Or type the name of your favorite team'
-        elsif Team.all.name.include("#{user_input}")
-            puts "Goals per game: " + (Team.all.find_by(name: "#{user_input}").goals_per_game).to_s 
-            puts "Win percentage: " + (Team.all.find_by(name: "#{user_input}").win_percentage).to_s
-            get_ranking(user_input)
-
-            puts "Type 'back' to go back to the main menu"
-            user_input = STDIN.gets.chomp
-            self.input(user_input)
-
-        elsif user_input == 'stats'
-            puts "Team with most wins: " + Team.team_with_most_wins.name
-            puts "Team with most away wins: " + Team.team_with_most_away_wins.name
-            puts "Arena with most goals: " + Arena.arena_with_most_goals 
-
-        elsif user_input == 'back'
-            start
-        else
-            puts "Invalid input. Please enter t to see a list of teams or type the name of your team"
-        end
-
-        def exit
-            puts "Until next time"
-        end
-
-    end
-
-
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,8 @@
 require_relative '../config/environment'
+require_all 'app/models'
+
 
 menu = Menu.new
 menu.start
 
-puts "HELLO WORLD"
+#puts "HELLO WORLD"
